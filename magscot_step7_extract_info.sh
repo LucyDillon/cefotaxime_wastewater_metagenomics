d5dbeab66cf107a7dcfc44afa6a02b8de7ae1e20
LucyDillon <62658632+LucyDillon@users.noreply.github.com>
2026-01-15 14:40:01 +0000

    Update magscot_step7_extract_info.sh

--- magscot_step7_extract_info.sh
+++ magscot_step7_extract_info.sh
@@ -4,7 +4,7 @@
 #SBATCH --mem=10G
 #SBATCH --mail-user=l.dillon@qub.ac.uk
 #SBATCH --mail-type=END,BEGIN,FAIL
-#SBATCH --error=hmmer-%A-%a.err
-#SBATCH --job-name=hmmer
+#SBATCH --error=extract_results-%A-%a.err
+#SBATCH --job-name=extract_results
 
 for i in $(cat samples.txt); do tail -n +2 magscot/${i}_magscot.refined.contig_to_bin.out | cut -f1 | sed 's#.*/##' | sort -u > magscot/${i}_cleanbins.txt; done
