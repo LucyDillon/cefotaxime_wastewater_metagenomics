ae2905da3b02bd765987719a35386ea97b8fcaac
LucyDillon <62658632+LucyDillon@users.noreply.github.com>
2026-01-15 13:53:39 +0000

    Create metaspades.sh

---
+++ metaspades.sh
@@ -0,0 +1,25 @@
+for R1 in preprocess/bowtie2_decontamination/*_unmapped_R1.fq.gz; do
+    SAMPLE=$(basename "$R1" _unmapped_R1.fq.gz)
+
+    sbatch --job-name=meta_${SAMPLE} <<EOF
+#!/bin/bash
+#SBATCH --partition=k2-medpri,k2-bioinf
+#SBATCH --time=23:59:00
+#SBATCH --mem=200G
+#SBATCH --cpus-per-task=32
+#SBATCH --output=metaspades-${SAMPLE}.out
+#SBATCH --error=metaspades-${SAMPLE}.err
+
+source activate /mnt/scratch2/igfs-anaconda/conda-envs/spades_4.0.0
+
+metaspades.py \
+  -1 preprocess/bowtie2_decontamination/${SAMPLE}_unmapped_R1.fq.gz \
+  -2 preprocess/bowtie2_decontamination/${SAMPLE}_unmapped_R2.fq.gz \
+  -o assembly/metaspades/${SAMPLE} \
+  -t 32 \
+  -k 21,33,55
+EOF
+
+done
+
+# NOTE: this script will submit a separate job to the slum queue
